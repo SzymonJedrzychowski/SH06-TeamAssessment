20ca8a9b80c25458d700e5c193642475b98d1d03
Fluets <Fluets@gmail.com>
2023-03-11 20:44:03 +0000

    add structure and basic nav

--- app/src/components/pages/Partner.js
+++ app/src/components/pages/Partner.js
@@ -8,11 +8,67 @@ import React, { useState, useEffect } from 'react';
  * @author Matthew Cartwright
  */
 
-function Partner(){
+const Partner = () => {
+
+    // On render hooks
+    useEffect(() => {
+        //TODO: fetch request for data
+        console.log("Render complete")
+    }, [])
+
+    // State variable hooks
+    const [showContribute, setShowcontribute] = useState(true);
+    const [showReview, setShowReview] = useState(false);
+    const [showPublished, setShowPublished] = useState(false);
+
+    // Other variables
+
+
+    // Functions
+     // -Navigation
+     const setContribute = () => {
+        setShowcontribute(true);
+        setShowReview(false);
+        setShowPublished(false);
+     }
+
+     const setReview = () => {
+        setShowcontribute(false);
+        setShowReview(true);
+        setShowPublished(false);
+     }
+
+     const setPublished = () => {
+        setShowcontribute(false);
+        setShowReview(false);
+        setShowPublished(true);
+     }
+
+
+
+    // Content
+
+     // -Contribute
+
+     // -Review
+
+     // -Published
+
     return(
-        <div>
-            <h1>Partner Page Skeleton</h1>
-            <p>Lorem Ipsum</p>
+        <div className = 'Partner'>
+            <div className = 'PartnerHeader'>
+                <h1>Partner Page Skeleton</h1>
+                <p>Lorem Ipsum</p>
+                <button onClick = {setContribute}></button>
+                <button onClick = {setReview}></button>
+                <button onClick = {setPublished}></button>
+            </div>
+            <div className = 'PartnerBody'>
+            {showContribute && <div className = 'PartnerContribute'> </div>}
+            {showReview && <div className = 'PartnerReview'> </div>}
+            {showPublished && <div className = 'PartnerPublished'> </div>}
+
+            </div>
         </div>
     )
 }
