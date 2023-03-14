0ed0900cb7bf964fecdcdc536a6ca594ff3e22d5
Szymon Jędrzychowski <czarmistrz@outlook.com>
2023-03-14 11:13:40 +0000

    fix to verify and authenticate

--- app/src/components/pages/Login.js
+++ app/src/components/pages/Login.js
@@ -1,47 +1,58 @@
-import FormData from 'form-data'; 
+import { useState } from 'react';
+import { Buffer } from 'buffer';
 
 function Login() {
-  const formData = new FormData();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-  const handleInputChange = (event) => {
-    formData.append(event.target.name, event.target.value);
-  //   for (var key of formData.entries()) {
-  //     console.log(key[0] + ', ' + key[1]);
-  // }
-  };
+  // Handler for username value.
+  const handleEmail = (event) => {
+    setEmail(event.target.value);
+  }
+
+  // Handler for password value.
+  const handlePassword = (event) => {
+    setPassword(event.target.value);
+  }
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    // console.log(JSON.stringify(Object.fromEntries(formData)));
+    const encodedString = Buffer.from(email + ":" + password).toString('base64');
     fetch("http://unn-w20027449.newnumyspace.co.uk/teamAssessment/api/authenticate", {
       method: "POST",
-      headers: {
-        "Content-Type": "multipart/form-data",
-      },
-      body: formData,
+      headers: new Headers({ "Authorization": "Basic " + encodedString })
     })
 
       .then((response) => response.json())
-      .then((data) => {
-        console.log("Success:", data);
-      })
+      .then(
+        (json) => {
+          if (json.message === "Success") {
+            console.log("Success: ");
+            localStorage.setItem('token', json.data.token);
+          } else {
+            console.log("Not success: ");
+            localStorage.removeItem('token');
+          }
+          console.log(json);
+        })
       .catch((error) => {
         console.error("Error:", error);
       });
-  };     
-  
+  };
+
   return (
     <form onSubmit={handleSubmit}>
       <label>
         Email:
-        <input type="text" name="email" onChange={handleInputChange} />
+        <input type="text" name="email" onChange={handleEmail} />
       </label>
       <br />
       <label>
         Password:
-        <input type="password" name="password" onChange={handleInputChange} />
+        <input type="password" name="password" onChange={handlePassword} />
       </label>
       <br />
-      
+
       <button type="submit">Log In</button>
     </form>
   );
